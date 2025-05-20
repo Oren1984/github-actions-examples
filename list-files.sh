09d181bb2df197ca0188fca3621baa4af6be1c15
Oren1984 <orensalmi1984@gmail.com>
2025-05-20 18:59:47 +0300

    update palindrome.py

--- list-files.sh
+++ list-files.sh
@@ -1,6 +1,4 @@
-@'
 #!/bin/bash
 echo "Listing all files with detailed info:"
 ls -la
-'@ | Out-File -Encoding utf8 -NoNewline list-files.sh
 
--- run-scripts.sh
+++ run-scripts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Starting run-scripts.sh - bash step"
-echo "Hello from the Bash script!"
+
+# הרצת סקריפט list-files.sh
+echo "Running list-files.sh:"
+./list-files.sh
+
+echo ""
+
+# הרצת סקריפט Python
+echo "Running palindrome.py:"
+python3 palindrome.py
